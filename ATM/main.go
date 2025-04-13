0c5400dc782e3a7f2bd4a6860881bfbdbb16fe24
adelina-chachanidze <adelina.chachanidze@gmail.com>
2025-04-13 13:48:23 +0300

    basic logic

--- ATM/main.go
+++ ATM/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main () {
-	balance := 1000
+	var balance float64 = 1000.00
 
 
 	fmt.Println("Welcome to the ATM!\nPlease select the action")
@@ -21,8 +21,36 @@ func main () {
 	//= is for assing a value to var. == for check for the quality
 	if choice == 1 {
 		fmt.Println("Your balance:", balance)
+
+	} else if choice == 2 {
+		fmt.Println("Enter your deposit: ")
+		var deposit float64
+		fmt.Scan(&deposit)
+			if deposit <=0 {
+				fmt.Println("Invalid amount. Minimal deposit: 0.01")
+				return
+			}
+		balance += deposit
+		fmt.Println("Deposit complete!\nYour new balance:", balance)
+
+	} else if choice == 3 {
+		fmt.Println("Enter your withdrawl amount: ")
+		var withdraw float64
+		fmt.Scan(&withdraw)
+			if withdraw <=0 {
+				fmt.Println("Invalid amount. Minimal withdrawl: 0.01")
+				return
+			}
+
+			if withdraw > balance {
+				fmt.Println("Not enough to withdraw, availabe balance:", balance)
+				return
+			} 
+			
+		balance -= withdraw
+		fmt.Println("Withdrawl complete!\nYour new balance:", balance)
+
+	} else {
+		fmt.Println("Thank you for using our ATM! Come back soon!")
 	}
-
-	fmt.Println("Your choice:", choice)
-
 }
