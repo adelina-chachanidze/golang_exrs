09da8fec19fc02cb08afb1872c84725827c91ad3
adelina-chachanidze <adelina.chachanidze@gmail.com>
2025-04-13 14:15:13 +0300

    added cases

--- ATM/main.go
+++ ATM/main.go
@@ -1,60 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main () {
+func main() {
 	var balance float64 = 1000.00
+
 	fmt.Println("Welcome to the ATM!")
 
 	for {
-	fmt.Println("Please select the action")
-	fmt.Println("\n1. Check your balance")
-	fmt.Println("2. Deposit")
-	fmt.Println("3. Withdraw")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("\nType 1-4 to choose: ")
-	fmt.Scan(&choice)
+		fmt.Println("\nPlease select the action")
+		fmt.Println("1. Check your balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
 
-	//toCheckBalance := choice == 1
+		var choice int
+		fmt.Print("Type 1-4 to choose: ")
+		fmt.Scan(&choice)
 
-	//= is for assing a value to var. == for check for the quality
-	if choice == 1 {
-		fmt.Println("Your balance:", balance)
+		switch choice {
+		case 1:
+			fmt.Println("Your balance:", balance)
 
-	} else if choice == 2 {
-		fmt.Println("Enter your deposit: ")
-		var deposit float64
-		fmt.Scan(&deposit)
-			if deposit <=0 {
+		case 2:
+			fmt.Println("Enter your deposit: ")
+			var deposit float64
+			fmt.Scan(&deposit)
+
+			if deposit <= 0 {
 				fmt.Println("Invalid amount. Minimal deposit: 0.01")
 				continue
 			}
-		balance += deposit
-		fmt.Println("\nDeposit complete!\nYour new balance:", balance)
 
-	} else if choice == 3 {
-		fmt.Println("\nEnter your withdrawl amount: ")
-		var withdraw float64
-		fmt.Scan(&withdraw)
-			if withdraw <=0 {
-				fmt.Println("\nInvalid amount. Minimal withdrawl: 0.01")
+			balance += deposit
+			fmt.Println("Deposit complete!\nYour new balance:", balance)
+
+		case 3:
+			fmt.Println("Enter your withdrawal amount: ")
+			var withdraw float64
+			fmt.Scan(&withdraw)
+
+			if withdraw <= 0 {
+				fmt.Println("Invalid amount. Minimal withdrawal: 0.01")
 				continue
 			}
 
 			if withdraw > balance {
-				fmt.Println("\nNot enough to withdraw, availabe balance:", balance)
+				fmt.Println("Not enough to withdraw, available balance:", balance)
 				continue
-			} 
-			
-		balance -= withdraw
-		fmt.Println("\nWithdrawl complete!\nYour new balance:", balance)
+			}
 
-	} else {
-		fmt.Println("Thank you for using our ATM! Come back soon!")
-		break
+			balance -= withdraw
+			fmt.Println("Withdrawal complete!\nYour new balance:", balance)
+
+		default:
+			fmt.Println("Thank you for using our ATM! Come back soon!")
+			return
+		}
 	}
-	}
-	
 }
